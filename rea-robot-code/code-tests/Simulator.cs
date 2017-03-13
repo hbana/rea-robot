f248570b510987f4c1389717647e821e29f5ed7c
Husain Bana <hsbana@gmail.com>
2017-03-13 22:18:52 +1100

    Finalised unit tests with appropirate assertions

--- rea-robot-code/code-tests/Simulator.cs
+++ rea-robot-code/code-tests/Simulator.cs
@@ -18,25 +18,36 @@ namespace code_tests
         [TestMethod]
         public void RotateLeft()
         {
-
+            Assert.IsTrue(oTabletop.Place("0,0,NORTH"));
+            Assert.IsTrue(oTabletop.Left());
+            Assert.AreEqual(oTabletop.Report(), "0,0,WEST");
         }
 
         [TestMethod]
         public void RotateRight()
         {
-
+            Assert.IsTrue(oTabletop.Place("0,0,NORTH"));
+            Assert.IsTrue(oTabletop.Right());
+            Assert.AreEqual(oTabletop.Report(), "0,0,EAST");
         }
 
         [TestMethod]
         public void MoveOneUnit()
         {
-
+            Assert.IsTrue(oTabletop.Place("0,0,NORTH"));
+            Assert.IsTrue(oTabletop.Move());
+            Assert.AreEqual(oTabletop.Report(), "0,1,NORTH");
         }
 
         [TestMethod]
-        public void ReportCurrentPosition()
+        public void MoveMultipleUnits()
         {
-
+            Assert.IsTrue(oTabletop.MoveRobot("PLACE 1,2,EAST"));
+            Assert.IsTrue(oTabletop.MoveRobot("MOVE"));
+            Assert.IsTrue(oTabletop.MoveRobot("MOVE"));
+            Assert.IsTrue(oTabletop.MoveRobot("LEFT"));
+            Assert.IsTrue(oTabletop.MoveRobot("MOVE"));
+            Assert.AreEqual(oTabletop.Report(), "3,3,NORTH");
         }
     }
 }
