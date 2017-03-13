444ef81ce40efe56489af29166d5c567d2a19486
Husain Bana <hsbana@gmail.com>
2017-03-13 14:00:20 +1100

    Initial solution commit

---
+++ rea-robot-code/rea-robot-code/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rea_robot_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
