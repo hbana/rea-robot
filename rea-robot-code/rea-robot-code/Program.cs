395bcae356aae02187357731fd23b1de22cecfb5
Husain Bana <hsbana@gmail.com>
2017-03-14 00:50:18 +1100

    Minor corrections to executing program and improved comments

--- rea-robot-code/rea-robot-code/Program.cs
+++ rea-robot-code/rea-robot-code/Program.cs
@@ -11,11 +11,15 @@ namespace rea_robot_code
 
             Console.WriteLine("Enter commands to place & move a robot on the 5x5 tabletop...\r\n");
 
-            // read commands in a succession from the command prompt and 
-            // move the robot accordingly
-            while (command.ToLowerInvariant() != "report")
+            // read commands in a succession from the command prompt and move 
+            // the robot accordingly until asked to report its final position
+            while (1==1)
             {
                 command = Console.ReadLine();
+
+                if (command.ToLowerInvariant() == "report")
+                    break;
+
                 if (!oRobotSimulator.MoveRobot(command))
                     Console.WriteLine("...Robot can't be moved further in this direction to prevent it from falling to destruction");
             }
