1f4d078f0cdfffb592526bb981548b6993aa7d6f
Husain Bana <hsbana@gmail.com>
2017-03-13 22:24:38 +1100

    Changed the name of robot simulator class in unit tests

--- rea-robot-code/code-tests/Simulator.cs
+++ rea-robot-code/code-tests/Simulator.cs
@@ -7,47 +7,47 @@ namespace code_tests
     [TestClass]
     public class Simulator
     {
-        private Tabletop oTabletop;
+        private RobotSimulator oRobotSimulator;
 
         [TestInitialize]
         public void TestSetup()
         {
-            oTabletop = new Tabletop();
+            oRobotSimulator = new RobotSimulator();
         }
 
         [TestMethod]
         public void RotateLeft()
         {
-            Assert.IsTrue(oTabletop.Place("0,0,NORTH"));
-            Assert.IsTrue(oTabletop.Left());
-            Assert.AreEqual(oTabletop.Report(), "0,0,WEST");
+            Assert.IsTrue(oRobotSimulator.Place("0,0,NORTH"));
+            Assert.IsTrue(oRobotSimulator.Left());
+            Assert.AreEqual(oRobotSimulator.Report(), "0,0,WEST");
         }
 
         [TestMethod]
         public void RotateRight()
         {
-            Assert.IsTrue(oTabletop.Place("0,0,NORTH"));
-            Assert.IsTrue(oTabletop.Right());
-            Assert.AreEqual(oTabletop.Report(), "0,0,EAST");
+            Assert.IsTrue(oRobotSimulator.Place("0,0,NORTH"));
+            Assert.IsTrue(oRobotSimulator.Right());
+            Assert.AreEqual(oRobotSimulator.Report(), "0,0,EAST");
         }
 
         [TestMethod]
         public void MoveOneUnit()
         {
-            Assert.IsTrue(oTabletop.Place("0,0,NORTH"));
-            Assert.IsTrue(oTabletop.Move());
-            Assert.AreEqual(oTabletop.Report(), "0,1,NORTH");
+            Assert.IsTrue(oRobotSimulator.Place("0,0,NORTH"));
+            Assert.IsTrue(oRobotSimulator.Move());
+            Assert.AreEqual(oRobotSimulator.Report(), "0,1,NORTH");
         }
 
         [TestMethod]
         public void MoveMultipleUnits()
         {
-            Assert.IsTrue(oTabletop.MoveRobot("PLACE 1,2,EAST"));
-            Assert.IsTrue(oTabletop.MoveRobot("MOVE"));
-            Assert.IsTrue(oTabletop.MoveRobot("MOVE"));
-            Assert.IsTrue(oTabletop.MoveRobot("LEFT"));
-            Assert.IsTrue(oTabletop.MoveRobot("MOVE"));
-            Assert.AreEqual(oTabletop.Report(), "3,3,NORTH");
+            Assert.IsTrue(oRobotSimulator.MoveRobot("PLACE 1,2,EAST"));
+            Assert.IsTrue(oRobotSimulator.MoveRobot("MOVE"));
+            Assert.IsTrue(oRobotSimulator.MoveRobot("MOVE"));
+            Assert.IsTrue(oRobotSimulator.MoveRobot("LEFT"));
+            Assert.IsTrue(oRobotSimulator.MoveRobot("MOVE"));
+            Assert.AreEqual(oRobotSimulator.Report(), "3,3,NORTH");
         }
     }
 }
