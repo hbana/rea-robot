620d9a5e7e0741810b925aeee4dd39b4f6549c95
Husain Bana <hsbana@gmail.com>
2017-03-13 21:17:15 +1100

    Initial unit test abstractions

---
+++ rea-robot-code/code-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("code-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("code-tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9e6070d-a8f0-4ef8-a64b-3ca6f4399ba0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ rea-robot-code/code-tests/Simulator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using rea_robot_code;
+
+namespace code_tests
+{
+    [TestClass]
+    public class Simulator
+    {
+        private Tabletop oTabletop;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            oTabletop = new Tabletop();
+        }
+
+        [TestMethod]
+        public void RotateLeft()
+        {
+
+        }
+
+        [TestMethod]
+        public void RotateRight()
+        {
+
+        }
+
+        [TestMethod]
+        public void MoveOneUnit()
+        {
+
+        }
+
+        [TestMethod]
+        public void ReportCurrentPosition()
+        {
+
+        }
+    }
+}
